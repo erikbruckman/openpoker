aa1bd1b7227e7b6316f6c57e3a70fe62d2b6155d
Erik Bruckman <erikbruckman@gmail.com>
2026-05-06 19:29:06 -0500

    Remove node modules and other bs. Perform project hygiene refactor.

--- dist/src/models/PlayerAction.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PlayerAction = void 0;
-var types_1 = require("../../shared/types");
-Object.defineProperty(exports, "PlayerAction", { enumerable: true, get: function () { return types_1.PlayerAction; } });
